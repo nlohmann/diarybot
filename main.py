f81f488177db5c4cfd2f44cfdcff18ca696e2dd7
Marcus Sümnick <marcus@suemnick.com>
2014-10-24 13:35:37 +0200

    +add foursquare-support

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ diarybot.sources.chat.skype.Skype()
 diarybot.sources.chat.imessage.Imessage()
 
 diarybot.sources.location.moves.Moves()
+diarybot.sources.location.foursquare.dataimport.FoursquareDataImport()
+
 diarybot.sources.social.twitter.Twitter()
 
 diarybot.sources.life.xbox.Xbox()
